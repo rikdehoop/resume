fa214aa00c82ad18046c0bb6362966811a6c57d4
rikdehoop <71008894+rikdehoop@users.noreply.github.com>
2025-04-15 21:11:04 +0200

    Update copy_and_print.js

--- copy_and_print.js
+++ copy_and_print.js
@@ -1,8 +1,22 @@
-var btn = document.getElementById("print");
-btn.onclick = () =>{
-    window.alert("Adjust the print setting Scale and Options!")
-    window.print()
-}
+const btn = document.getElementById("print");
+
+btn.onclick = () => {
+window.alert("Generating PDF of the whole page...");
+
+// Select the whole body to convert
+const element = document.body;
+
+const opt = {
+  margin:       0,
+  filename:     'webpage.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { scale: 2 },
+  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+};
+
+// Convert and save
+html2pdf().set(opt).from(element).save();
+};
 var copyText = document.getElementById("copy");
 copyText.onclick = () =>{
     window.alert("The page has been copied to your clipboard!")
