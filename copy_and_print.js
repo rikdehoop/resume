79b61d03b22d8183f3e768793b8eeadfc41aee9b
rikdehoop <71008894+rikdehoop@users.noreply.github.com>
2025-04-15 23:14:14 +0200

    Update copy_and_print.js

--- copy_and_print.js
+++ copy_and_print.js
@@ -1,28 +1,15 @@
 const btn = document.getElementById("print");
 
 btn.onclick = () => {
-  window.alert("Je CV wordt gegenereerd als PDF...");
+  const link = document.createElement('a');
+  window.alert("The page has been downloaded as pdf!")
+  link.href = 'https://raw.githubusercontent.com/rikdehoop/resume/main/img/CV_Rik_de_Hoop.pdf';
+  link.download = 'CV_Rik_de_Hoop.pdf'; // this is the filename for download
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
-  // Hide buttons before printing
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach(btn => btn.style.display = "none");
-
-  const element = document.querySelector(".print-area");
-
-  const opt = {
-    margin:       [5, 5, 5, 5], // top, left, bottom, right
-    filename:     'CV_Rik_de_Hoop.pdf',
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 2, useCORS: true },
-    jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
-    pagebreak:    { mode: ['avoid-all', 'css', 'legacy'] }
-  };
-
-  html2pdf().set(opt).from(element).save().then(() => {
-    // Show buttons again
-    buttons.forEach(btn => btn.style.display = "inline-block");
-  });
-}
 var copyText = document.getElementById("copy");
 copyText.onclick = () =>{
     window.alert("The page has been copied to your clipboard!")
